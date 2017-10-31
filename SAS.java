0f786339cc7f2503c74963b7edd76bda12b2ca86
Sladyn <gunnerforlife00@gmail.com>
2017-10-31 12:31:59 +0530

    Add files via upload

--- SAS.java
+++ SAS.java
@@ -25,17 +25,34 @@ class text_area extends JFrame
       public ArrayList<JPanel> panelNo = new ArrayList<JPanel>();
       public int textAreaCounter = 0;
       public int panelCounter = 0;
+      public boolean savedStatus = false;
+     // public String[] fontOptions = {"Serif", "Agency FB", "Arial", "Calibri", "Cambrian", "Century Gothic", "Comic Sans MS"};
 
+    // JComboBox <String> fontList = new JComboBox<>(fontOptions);
 
 
 
         JTextArea textArea,textArea1;
         JScrollPane scrollpane,scrollpane1;
         JMenuBar menuBar;
-        JMenu menu,menu1;
-        JMenuItem newAction,saveAction,openAction,closeAction,undoAction,redoAction;
+        JMenu menu,menu1,menu2;
+        JMenuItem newAction,saveAction,openAction,closeAction,undoAction,redoAction,BoldAction,FontFamily,Serif,courier;
         JTabbedPane tabbedPane,tabbedPane1;
-         
+        JComboBox box;
+        JLabel label;
+
+
+
+
+        public void setsavedStatus(boolean savedStatus)
+        {
+          this.savedStatus = savedStatus;
+        }
+
+        public boolean getsavedStatus()
+        {
+          return savedStatus;
+        }
         
 
 
@@ -57,6 +74,7 @@ class text_area extends JFrame
            public void stateChanged(ChangeEvent evt) 
            {
               JTabbedPane tabbedPane = (JTabbedPane)evt.getSource();
+
            }
 
            });
@@ -70,16 +88,16 @@ class text_area extends JFrame
 
         //calls create menu
          create_menu();
+         setVisible(true);
         }
 
         public void create_menu()
         {
                 menuBar = new JMenuBar();
+
                 menu = new JMenu("FILE");
-                
                 menu1 = new JMenu("Edit");
-
-
+                menu2 = new JMenu("Font");
                 newAction = new JMenuItem("New");
 
 
@@ -103,15 +121,18 @@ class text_area extends JFrame
          {
                 public void actionPerformed(ActionEvent e)
                 {
+                      
                         JFileChooser fileChooser = new JFileChooser();
                         fileChooser.setDialogTitle("Specify new File");
                         fileChooser.setSelectedFile(new File("file1"));
                         int userSelection = fileChooser.showSaveDialog(fileChooser);
                          
-                         
-                        System.out.println("The selected tab is"+tabbedPane.getSelectedIndex());
+                        
+
+                        //System.out.println("The selected tab is"+tabbedPane.getSelectedIndex());
                         if(userSelection == JFileChooser.APPROVE_OPTION)
                         {
+                                setsavedStatus(true);
                                 File file = fileChooser.getSelectedFile();
                                 
                                 BufferedWriter  writer = null;
@@ -128,7 +149,7 @@ class text_area extends JFrame
                                         {
                                          UIManager.put("OptionPane.minimumSize",new Dimension(300,300)); 
                                          String message = "Please enter a file type\n";
-                                         JOptionPane.showMessageDialog(null,message,"title",JOptionPane.WARNING_MESSAGE);
+                                         JOptionPane.showConfirmDialog(null,message,"title",JOptionPane.WARNING_MESSAGE);
                                         }
                                         writer.write(textAreaNo.get(tabbedPane.getSelectedIndex()).getText());
                                         writer.close();
@@ -150,7 +171,22 @@ class text_area extends JFrame
                 {
                    public void actionPerformed(ActionEvent e)
                    {
+
+                     if(savedStatus)
                      tabbedPane.removeTabAt(tabbedPane.getSelectedIndex());
+
+                      else
+                      {
+                      UIManager.put("OptionPane.minimumSize",new Dimension(300,100)); 
+                       String message = "This file is not saved";
+                       int ans = JOptionPane.showConfirmDialog(null,"This file is not saved.\nAre you sure you want to close?");
+
+                       if(ans ==JOptionPane.YES_OPTION)
+                       {
+                        tabbedPane.removeTabAt(tabbedPane.getSelectedIndex());
+                       }
+                      }
+
                    }
                 });
 
@@ -164,6 +200,8 @@ class text_area extends JFrame
 
                  public void actionPerformed(ActionEvent e )
                  {
+                       // System.out.println("Hello");
+
                       JFileChooser fileChooser = new JFileChooser();
                       int ans = fileChooser.showOpenDialog(fileChooser);
                       if(ans == JFileChooser.APPROVE_OPTION)
@@ -242,18 +280,74 @@ class text_area extends JFrame
                   }
                 });
 
-                
-        
+
+                  Serif = new JMenuItem("Serif");
+                  Serif.addActionListener(new ActionListener()
+                  {
+                    public void actionPerformed(ActionEvent ev2)
+                    {
+                      textAreaNo.get(tabbedPane.getSelectedIndex()).setFont(new Font("Serif",Font.PLAIN,12));
+                    }
+                  });
+
+
+                 JMenuItem Agency = new JMenuItem("Agency FB");
+                  Agency.addActionListener(new ActionListener()
+                  {
+                    public void actionPerformed(ActionEvent ev3)
+                    {
+                      textAreaNo.get(tabbedPane.getSelectedIndex()).setFont(new Font("Agency FB",Font.PLAIN,12));
+                    }
+                  });
+
+
+                  courier = new JMenuItem("Courier");
+                  courier.addActionListener(new ActionListener()
+                  {
+                    public void actionPerformed(ActionEvent ev4)
+                    {
+                      textAreaNo.get(tabbedPane.getSelectedIndex()).setFont(new Font("Courier",Font.BOLD,12));
+                    }
+                  });
+
+                   JMenuItem Cambrian = new JMenuItem("Cambrian");
+                  Cambrian.addActionListener(new ActionListener()
+                  {
+                    public void actionPerformed(ActionEvent ev3)
+                    {
+                      textAreaNo.get(tabbedPane.getSelectedIndex()).setFont(new Font("Cambrian",Font.PLAIN,12));
+                    }
+                  });
+
+              
+                  JMenuItem selectAction = new JMenuItem("Select All");
+                  selectAction.addActionListener(new ActionListener()
+                  {
+                    public void actionPerformed(ActionEvent ev4)
+                    {
+                     textAreaNo.get(tabbedPane.getSelectedIndex()).requestFocus();
+                     textAreaNo.get(tabbedPane.getSelectedIndex()).selectAll();
+                     }
+                  });
+
+               
+
 
 
                 menuBar.add(menu);
                 menuBar.add(menu1);
+                menuBar.add(menu2);
                 menu.add(newAction);
                 menu.add(saveAction);
                 menu.add(openAction);
                 menu.add(closeAction);
                 menu1.add(undoAction);
                 menu1.add(redoAction);
+                menu1.add(selectAction);
+                menu2.add(Serif);
+                menu2.add(Agency);
+                menu2.add(courier);
+                menu2.add(Cambrian);
                 setJMenuBar(menuBar);
 
 
@@ -270,7 +364,7 @@ class text_area extends JFrame
                     textArea.setEditable(true);
                     textArea.setFont(new Font("Serif",Font.ITALIC,15));
                     textAreaNo.add(textArea);
-
+                    setsavedStatus(false);
           
                     JPanel panel = new JPanel();
                     panel.setLayout(new BorderLayout());
